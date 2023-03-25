97cad8d17217d578adc195fdede357c82939552b
BABASTAS728 <Aberezkinn954@gmail.com>
2023-03-25 23:48:19 +0300

    chat commit 2

--- Chat/src/main/kotlin/Client.kt
+++ Chat/src/main/kotlin/Client.kt
@@ -2,40 +2,46 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.Socket
+import java.net.UnknownHostException
 
 fun main() {
-    val serverAddress = "localhost" // здесь указываем адрес сервера
-    val serverPort = 8000 // здесь указываем порт сервера
+    print("Введите IP-адрес сервера: ")
+    val serverAddress = readlnOrNull() ?: ""
+    print("Введите порт сервера: ")
+    val serverPort = readlnOrNull()?.toIntOrNull() ?: 0
+    try {
+        val socket = Socket(serverAddress, serverPort)
+        println("Успешно подключились к серверу")
 
-    val socket = Socket(serverAddress, serverPort)
-    println("Успешно подключились к серверу")
+        val input = BufferedReader(InputStreamReader(System.`in`))
+        val output = PrintWriter(socket.getOutputStream(), true)
+        val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
 
-    val input = BufferedReader(InputStreamReader(System.`in`))
-    val output = PrintWriter(socket.getOutputStream(), true)
-    val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
-
-    Thread(
-        Runnable {
-            try {
-                while (true) {
-                    val message = reader.readLine() ?: break
-                    println(message)
+        Thread(
+            Runnable {
+                try {
+                    while (true) {
+                        val message = reader.readLine() ?: break
+                        println(message)
+                    }
+                } catch (e: Exception) {
+                    println("Ошибка при чтении сообщения от сервера: ${e.message}")
+                } finally {
+                    reader.close()
                 }
-            } catch (e: Exception) {
-                println("Ошибка при чтении сообщения от сервера: ${e.message}")
-            } finally {
-                reader.close()
             }
+        ).apply { start() }
+
+        var message: String
+        while (true) {
+            message = input.readLine() ?: break
+            output.println(message)
         }
-    ).apply { start() }
 
-    var message: String
-    while (true) {
-        message = input.readLine() ?: break
-        output.println(message)
+        output.close()
+        input.close()
+        socket.close()
+    } catch (e: UnknownHostException) {
+        println("Неизвестный адрес сервера.")
     }
-
-    output.close()
-    input.close()
-    socket.close()
 }
--- Chat/src/main/kotlin/Server.kt
+++ Chat/src/main/kotlin/Server.kt
@@ -7,35 +7,43 @@ import java.util.*
 
 fun main() {
     val port = 8000
-    val serverSocket = ServerSocket(port)
-    val clients = Collections.synchronizedList(mutableListOf<Socket>())
-    println("Сервер запущен на порту $port")
+    try {
+        val serverSocket = ServerSocket(port)
+        if (serverSocket.isBound) {
+            println("Порт $port уже занят.")
+        } else {
+            println("Сервер запущен на порту $port")
+            val clients = Collections.synchronizedList(mutableListOf<Socket>())
 
-    while (true) {
-        val clientSocket = serverSocket.accept()
-        println("Новое соединение: ${clientSocket.inetAddress.hostAddress}")
-        clients.add(clientSocket)
+            while (true) {
+                val clientSocket = serverSocket.accept()
+                println("Новое соединение: ${clientSocket.inetAddress.hostAddress}")
+                clients.add(clientSocket)
 
-        Thread(
-            Runnable {
-                try {
-                    val reader = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+                Thread(
+                    Runnable {
+                        try {
+                            val reader = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
 
-                    while (true) {
-                        val message = reader.readLine() ?: break
-                        println("Получено сообщение от ${clientSocket.inetAddress.hostAddress}: $message")
-                        sendToAllClients(message, clients, clientSocket)
+                            while (true) {
+                                val message = reader.readLine() ?: break
+                                println("Получено сообщение от ${clientSocket.inetAddress.hostAddress}: $message")
+                                sendToAllClients(message, clients, clientSocket)
+                            }
+                            reader.close()
+                        } catch (e: Exception) {
+                            println("Ошибка при обработке соединения: ${e.message}")
+                        } finally {
+                            clientSocket.close()
+                            clients.remove(clientSocket)
+                            println("Соединение закрыто: ${clientSocket.inetAddress.hostAddress}")
+                        }
                     }
-                    reader.close()
-                } catch (e: Exception) {
-                    println("Ошибка при обработке соединения: ${e.message}")
-                } finally {
-                    clientSocket.close()
-                    clients.remove(clientSocket)
-                    println("Соединение закрыто: ${clientSocket.inetAddress.hostAddress}")
-                }
+                ).apply { start() }
             }
-        ).apply { start() }
+        }
+    } catch (e: Exception) {
+        println("Ошибка при создании серверного сокета: ${e.message}")
     }
 }
 
